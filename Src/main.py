c75cb34547c85b91898d5160832d1563b92fa77b
Wajid <errormrcoder@example.com>
2025-08-16 13:59:40 +0500

    Write code to generate qr code image.

---
+++ Src/main.py
@@ -0,0 +1,21 @@
+import qrcode
+
+# Data you want to encode
+data = "https://github.com/wajidkorkani"
+
+# Create qr object
+qr = qrcode.QRCode(
+    version=1,  # size of the QR Code (1–40), higher = bigger and more data
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # error correction level
+    box_size=10,  # size of each box in pixels
+    border=4,     # border thickness (boxes)
+)
+
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create image
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save image
+img.save("qr_code.png")
